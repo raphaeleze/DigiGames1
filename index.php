8cd5696e50a6df3835898623743721c327b304f7
darlingdaniel4790 <58652962+darlingdaniel4790@users.noreply.github.com>
2020-04-10 18:57:10 +0300

    This is the beginning

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Test Project</title>
+    <style media="screen">
+      h1{
+        text-align: center;
+        font-weight: bold;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>This is just a test...</h1>
+    <form class="" action="index.html" method="post">
+      <input type="email" name="email" placeholder="Your Email">
+      <input type="password" name="password" placeholder="Your Password">
+      <button type="button" name="button">Submit Answers</button>
+    </form>
+  </body>
+</html>
